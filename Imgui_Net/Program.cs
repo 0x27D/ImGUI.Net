76790cb62161f530909915ca487a8d781cdbbe34
0xTheX <huynguyenit2k6@gmail.com>
2023-08-30 15:27:32 +0700

    first commit

---
+++ Imgui_Net/Program.cs
@@ -0,0 +1,69 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using static System.Net.WebRequestMethods;
+using System.ComponentModel;
+using System.Reflection;
+using Veldrid;
+
+namespace Imgui_Net
+{
+    public class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdWindow);
+       const int hide = 0;
+        const int show = 1;
+        public static void Main(string[] args)
+        {
+            RenderClass.KeyAuthApp.init();
+            IntPtr handle = GetConsoleWindow();
+
+            ShowWindow(handle,hide);
+            
+            RenderClass ove = new RenderClass();
+                ove.Start().Wait();
+            
+           
+
+
+
+        }
+        public static void ProgressBarCiz(int sol, int ust, int deger, int isaret, ConsoleColor color)
+        {
+            char[] symbol = new char[5] { '\u25A0', '\u2592', '\u2588', '\u2551', '\u2502' };
+            int maxBarSize = Console.BufferWidth - 1;
+            int barSize = deger;
+            decimal f = 1;
+            if (barSize + sol > maxBarSize)
+            {
+                barSize = maxBarSize - (sol + 5); // first 5 character "%100 "
+                f = (decimal)deger / (decimal)barSize;
+            }
+            Console.CursorVisible = false;
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(sol + 5, ust);
+            for (int i = 0; i < barSize + 1; i++)
+            {
+                System.Threading.Thread.Sleep(10);
+                Console.Write(symbol[isaret]);
+                Console.SetCursorPosition(sol, ust);
+                Console.Write((i * f).ToString("0,0")+ "%");
+                Console.SetCursorPosition(sol + 5 + i, ust);
+                
+            }
+            Console.ResetColor();
+        }
+
+
+
+
+
+    }
+}
---
+++ Imgui_Net/Render.cs
@@ -0,0 +1,184 @@
+﻿using ImGuiNET;
+using ClickableTransparentOverlay;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Memory;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Reflection;
+using Veldrid;
+
+namespace Imgui_Net
+{
+    public class RenderClass : Overlay
+    {
+        /// <summary>
+        /// https://keyauth.cc/
+        /// </summary>
+        public static api KeyAuthApp = new api(
+         name: "",                              
+         ownerid: "",
+         secret: "",
+         version: ""
+     );
+        Vector4 selected_color = new Vector4(1.0f,1.0f,1.0f,1.0f);
+        bool checkbox1;
+        bool checkbox2= false;
+       
+        bool checkbox;
+        string tyeLog = "Aimbot";
+      
+        bool checkboxexample2 = false;
+        bool checkboxexample;
+        bool Login_form = true;
+        bool Menu_form = false;
+        Vector2 Login_imgui = new Vector2(750, 500);
+        Vector2 Imgui_Size = new Vector2(350, 350);
+        Vector2 Menu_Size = new Vector2(510, 370);
+        Vector2 menu_login = new Vector2(20, 30);
+        Vector2 Node = new Vector2(3, 3);
+        Vector2 Node2 = new Vector2(5, 5);
+        string username = "";
+        string pass = "";
+        float rouding = 1.0f;
+        float opacity = 0.7f;
+        
+        protected override async void Render()
+        {
+            
+            if (Login_form)
+            {
+               
+                ImGuiStylePtr stylea = ImGui.GetStyle();
+                stylea.Alpha = 0.9f;
+                stylea.WindowBorderSize = 0.2f;
+                stylea.WindowRounding = 8f;
+                stylea.ChildRounding = 8f;
+             
+                stylea.Colors[(int)ImGuiCol.Border] = selected_color;
+                stylea.Colors[(int)ImGuiCol.Text] = selected_color;
+                stylea.Colors[(int)ImGuiCol.TitleBgActive] = new Vector4(0.50f,0.9f,0.40f,1.0f);
+                
+                ImGui.GetIO().Fonts.AddFontFromFileTTF(@"C:\Windows\Fonts\comicbd.ttf", 13);
+                
+
+                ImGui.SetNextWindowSize(Imgui_Size);
+                ImGui.SetNextWindowPos(Login_imgui, ImGuiCond.Appearing);
+                ImGui.Begin("bimsocool .Net Imgui", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoSavedSettings);
+                {
+                    ImGui.SetCursorPos(menu_login);
+                    ImGui.BeginChild("#bimsocool");
+                    {
+                        ImGui.Text("User Name");
+                        ImGui.InputText("##User", ref username, Char.MaxValue);
+                        ImGui.Text("Password");
+                        ImGui.InputText("##Pass", ref pass, Char.MaxValue, ImGuiInputTextFlags.Password);
+                        ImGui.Spacing();
+                        
+                        if (ImGui.Button("Login"))
+                        {
+                            
+                            Thread t2 = new Thread(() =>
+                            {
+                                Program.ProgressBarCiz(0, 8, 100, 0, ConsoleColor.DarkGreen);
+                                
+                              
+
+                            }); t2.Start();
+                            Thread t = new Thread(() =>
+                            {
+                                
+                                
+                                KeyAuthApp.login(username, pass);
+                                if (KeyAuthApp.response.success)
+                                {
+                                   
+                                   
+                                    Login_form = false;
+                                    Menu_form = true;
+
+                                }
+                                else
+                                {
+                                   
+                                }
+                                
+                            }); t.Start();
+
+                            
+                        }
+                        if (ImGui.IsItemHovered(ImGuiHoveredFlags.AllowWhenOverlapped))
+                        {
+                            ImGui.SetTooltip("Click Login");
+                        }
+                        ImGui.End();
+                        
+
+                    }
+                    ImGui.EndChild();
+
+
+                }
+                ImGui.End();
+            }
+            if(Menu_form)
+            {
+
+                ImGuiStylePtr style =  ImGui.GetStyle();
+                ImGuiStylePtr stylea = ImGui.GetStyle();
+                
+                stylea.WindowBorderSize = 0.2f;
+                stylea.WindowRounding = rouding;
+                stylea.ChildRounding = rouding;
+
+                stylea.Colors[(int)ImGuiCol.Border] = selected_color;
+                stylea.Colors[(int)ImGuiCol.Text] = selected_color;
+                style.Alpha = opacity;
+                style.WindowBorderSize = 0.2f;
+                ImGui.SetNextWindowSize(Menu_Size);
+                ImGui.SetNextWindowPos(Login_imgui, ImGuiCond.Appearing);
+                ImGui.Begin("bimsocool .Net Imgui", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoSavedSettings);
+                {
+                    
+                    ImGui.SetCursorPos(menu_login);
+                    ImGui.BeginChild("#test");
+                    {
+                        ImGui.ColorEdit4("Change Color", ref selected_color,ImGuiColorEditFlags.PickerMask| ImGuiColorEditFlags.NoInputs);
+                        ImGui.SliderFloat("Change Rouding", ref rouding,1,20);
+                        ImGui.SliderFloat("Change Opacity", ref opacity, 0.1f, 1.0f);
+                        ImGui.Spacing();
+                        ImGui.PushStyleVar(ImGuiStyleVar.FramePadding,Node);
+                        ImGui.PushStyleVar(ImGuiStyleVar.ItemInnerSpacing,Node2);
+                        checkboxexample = ImGui.Checkbox("Check Box", ref checkboxexample2);
+                        ImGui.PopStyleVar();
+                        ImGui.Spacing();
+                        ImGui.Spacing();
+
+                       
+
+
+                    } 
+                    ImGui.EndChild();
+
+                    
+                }
+                ImGui.End();
+            }
+            
+
+        }
+
+       
+
+
+
+
+
+
+        
+        
+       
+
+    }
+}
